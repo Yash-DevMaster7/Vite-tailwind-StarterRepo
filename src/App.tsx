a70ea4e014f681a52e206a073e79fbdf65736881
Yash Rudra <rudrayash007@gmail.com>
2024-12-14 02:15:16 +0530

    initialize repo

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mt-10 text-center font-semibold underline text-3xl">
+        Vite + Tailwind CSS Starter Repo
+      </div>
+      <div className="mt-5 text-center font-medium text-xl">
+        " Using Typescript "
+      </div>
+    </>
+  );
+}
+
+export default App;
